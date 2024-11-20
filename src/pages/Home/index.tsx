d6ad58d57c5331421123caf4f9316c0361cd29a4
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-20 17:13:35 -0300

    Add: Typescript in Zod Type

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,18 +7,27 @@ import { Play } from "phosphor-react";
 import { HomeContainer, FormContainer, TaskInput, MinutesAmountInput, CountdownContainer, Separator, StartCountdownButton } from "../../pages/Home/styles";
 
 const newCycleFormValidationSchema = zod.object({
-    task: zod.string().min(1, "Informe a tarefa"),
-    minutesAmount: zod.number()
+    task: zod
+        .string()
+        .min(1, "Informe a tarefa"),
+    minutesAmount: zod
+        .number()
         .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
         .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 export function Home() {
-    const { register, handleSubmit, watch }  = useForm({
+    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: "",
+            minutesAmount: 0,
+        }
     });
 
-    function handleCreateNewCycle(data: any) {
+    function handleCreateNewCycle(data: NewCycleFormData) {
         console.log(data);
     }
 
@@ -53,7 +62,7 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
-                        {...register("minutesAmount", { valueAsNumber: true } )}
+                        {...register("minutesAmount", { valueAsNumber: true })}
                     />
 
                     <span>minutos.</span>
