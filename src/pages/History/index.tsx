36f0904f5366061e6b8f350a6640486156fbd71a
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-23 20:34:58 -0300

    Adjust: Date to ptBR in History Page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react"
 import { CyclesContext } from "../../contexts/CyclesContext"
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { HistoryContainer, HistoryList, Status } from "./styles"
 
@@ -27,7 +29,11 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR
+                                    })}
+                                    </td>
                                     <td>
                                         {
                                             cycle.finishedDate && <Status statusColor="green">Concluído</Status>
