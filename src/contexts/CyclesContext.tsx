3fbacb600ccc7b78e069a363f3eb7892a5129c6f
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-25 15:09:20 -0300

    Adjust: Separes dispatch logic

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useReducer, useState } from "react";
-import { Cycle, cyclesReducer, ActionTypes } from "../reducers/cycles";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
 
 interface CreateCycleData {
     task: string;
@@ -40,12 +41,7 @@ export function CyclesContentProvider({ children }: CyclesContextProviderProps) 
     }
 
     function markCurrentCycleAsFinished() {
-        dispatch({
-            type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-            payload: {
-                activeCycleId,
-            },
-        })
+        dispatch(markCurrentCycleAsFinishedAction());
 
     }
 
@@ -59,23 +55,13 @@ export function CyclesContentProvider({ children }: CyclesContextProviderProps) 
             startDate: new Date(),
         }
 
-        dispatch({
-            type: ActionTypes.ADD_NEW_CYCLE,
-            payload: {
-                newCycle,
-            },
-        })
+        dispatch(addNewCycleAction(newCycle));
 
         setAmountSecondsPassed(0);
     }
 
     function interruptCurrentCycle() {
-        dispatch({
-            type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-            payload: {
-                activeCycleId,
-            },
-        })
+        dispatch(interruptCurrentCycleAction());
     }
 
     return (
